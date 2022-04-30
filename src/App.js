b9ff12886c2da4c28d9dd3dafd101c481a7dbc53
asherjay <70672377+asherjay@users.noreply.github.com>
2022-04-30 11:32:36 +0900

    Passing Props to Class and Functional Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const Body = props => {
+  return (
+    <div>
+  <h1>{props.text}</h1>
+  <h1>{props.text2}</h1>
+  <h1>{props.myFunc(133, 456)}</h1>
+  </div>
+  )
+};
+class Header extends Component {
+  render () {
+  return (
+    <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1 className="App-title">{this.props.title}</h1>
+    <div>{this.props.myObj.a}</div>
+    <div>{JSON.stringify(this.props.myObj.b)}</div>
+    <div>{this.props.myArr[3]}</div>
+    <div>{this.props.myFunc(10, 12)}</div>
+  </header>
+  )
+  }
+}
+
+class App extends Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="Hello from App" 
+      num={5} 
+      myArr={[1,2,3,4,5]}
+      myFunc={(a, b) => a + b } 
+      myObj={{
+        a: 5,
+        b: 6
+      }} 
+      />
+      <Body
+      myFunc={(a, b) => a + b } 
+       text = "I am the body" 
+       text2="I am also a body"/>
+        <Body
+      myFunc={(a, b) => a + b } 
+       text = "I am the body" 
+       text2="I am also a body"/>
     </div>
   );
 }
+}
 
 export default App;
+
+// Passing Data to a Class Component 
